cbe7b649e5180e257fd209f3f9860b8c7666467c
Ben Hencke <hencke@gmail.com>
2023-10-10 09:37:29 -0700

    source maps for dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -7,70 +7,77 @@ const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").def
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const SizePlugin = require('size-plugin');
 
-const config = {
-  resolve: {
-    alias: {
-      react: "preact/compat",
-      "react-dom": "preact/compat",
+module.exports = function (env, argv) {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    //enable map file generation when in development mode
+    devtool: isProduction ? false : "source-map",
+
+    resolve: {
+      alias: {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+      },
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: "body",
-      scriptLoading: "blocking",
-      template: "src/index.html",
-    } ),
-    new MiniCssExtractPlugin(),
-    new HtmlInlineScriptPlugin(),
-    new HTMLInlineCSSWebpackPlugin(),
-    new CssMinimizerPlugin(),
-    new SizePlugin(),
-  ],
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|svg)$/,
-        type: "asset/inline",
-        parser: { dataUrlCondition: { maxSize: 1024*1024 } },
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            // browserlist defaults, excluding IE 11. IE 11 is responsible for a lot of polyfill!
-            targets: "> .5%, last 2 versions, Firefox ESR, not dead, not ie 11",
-            presets: [
-              "@babel/preset-react", 
-              ["@babel/preset-env", { bugfixes: true}]],
-            plugins: [
-              [
-                "@babel/plugin-transform-react-jsx",
-                {
-                  pragma: "h",
-                  pragmaFrag: "Fragment",
-                },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: "body",
+        scriptLoading: "blocking",
+        template: "src/index.html",
+      }),
+      new MiniCssExtractPlugin(),
+      new HtmlInlineScriptPlugin(),
+      new HTMLInlineCSSWebpackPlugin(),
+      new CssMinimizerPlugin(),
+      new SizePlugin(),
+    ],
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpg|svg)$/,
+          type: "asset/inline",
+          parser: { dataUrlCondition: { maxSize: 1024 * 1024 } },
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              // browserlist defaults, excluding IE 11. IE 11 is responsible for a lot of polyfill!
+              targets: "> .5%, last 2 versions, Firefox ESR, not dead, not ie 11",
+              presets: [
+                "@babel/preset-react",
+                ["@babel/preset-env", { bugfixes: true }]],
+              plugins: [
+                [
+                  "@babel/plugin-transform-react-jsx",
+                  {
+                    pragma: "h",
+                    pragmaFrag: "Fragment",
+                  },
+                ],
               ],
-            ],
+            },
           },
         },
-      },
-    ]
-  },
+      ]
+    },
+  };
 };
 
-module.exports = config;
+
